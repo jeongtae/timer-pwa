b3c8e6bc9110e18e575b8b1c7968381495c0b3e3
jeongtae <rsatang5@naver.com>
2020-02-12 00:21:20 +0900

    Style Home

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -3,10 +3,25 @@ import styles from "./Home.module.scss";
 import { Link } from "react-router-dom";
 
 export default function() {
+  const apps = ["Timer", "Stopwatch", "Speedometer", "Something", "Else", "And so on"];
   return (
-    <>
-      <p>Home</p>
-      <Link to="timer">Timer</Link>
-    </>
+    <div className={styles.container}>
+      <header>Web Apps</header>
+      <main>
+        {/* <Link to="timer">Timer</Link> */}
+        {apps.map((v, i) => (
+          <Link key={i} to="timer">
+            <img src="/logo192.png" alt="logo" />
+            <span>{v}</span>
+          </Link>
+        ))}
+      </main>
+      <footer>
+        Made by&nbsp;
+        <a href="https://github.com/jeongtae" target="_blank" rel="noopener noreferrer">
+          Jeongtae Kim
+        </a>
+      </footer>
+    </div>
   );
 }
