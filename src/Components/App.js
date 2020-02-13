6f459cefb24b1f9f0de6695380df278295540398
jeongtae <rsatang5@naver.com>
2020-02-13 22:43:07 +0900

    Add MultiProvider

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,15 +3,21 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, Timer } from "Routes";
 import { TimerProvider } from "Contexts";
 
+const MultiProvider = ({ providers = [], children }) => {
+  const reducer = (previous, provider) => React.createElement(provider, { children: previous });
+  const reduced = providers.reduce(reducer, children);
+  return reduced;
+};
+
 export default function App() {
   return (
-    <TimerProvider>
+    <MultiProvider providers={[TimerProvider]}>
       <Router>
         <Switch>
           <Route exact={true} path={"/"} component={Home} />
           <Route path={"/timer"} component={Timer} />
         </Switch>
       </Router>
-    </TimerProvider>
+    </MultiProvider>
   );
 }
