87de2ebe37a9f3458c9cf7fe81e0592dc08b7d23
jeongtae <rsatang5@naver.com>
2020-02-13 22:25:39 +0900

    Apply TimerContext Provider

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "Routes/Home";
 import Timer from "Routes/Timer";
+import { TimerProvider } from "Contexts";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact={true} path={"/"} component={Home} />
-        <Route path={"/timer"} component={Timer} />
-      </Switch>
-    </Router>
+    <TimerProvider>
+      <Router>
+        <Switch>
+          <Route exact={true} path={"/"} component={Home} />
+          <Route path={"/timer"} component={Timer} />
+        </Switch>
+      </Router>
+    </TimerProvider>
   );
 }
