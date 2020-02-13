404ea2309fd9e4355832487478fd0b9a164e1352
jeongtae <rsatang5@naver.com>
2020-02-13 22:24:54 +0900

    Update Timer.js

--- src/Routes/Timer/Timer.js
+++ src/Routes/Timer/Timer.js
@@ -1,11 +1,36 @@
 import React from "react";
 import styles from "./Timer.module.scss";
+import { useTimerContext } from "Contexts";
 
 export default function() {
+  const { state, startTimer, pauseTimer, resetTimer } = useTimerContext();
+
+  // Click event handler
+  const onClickStart = e => {
+    if (state === "start") {
+      pauseTimer();
+    } else {
+      startTimer();
+    }
+  };
+
+  // Click event handler
+  const onClickReset = e => {
+    resetTimer();
+  };
+
   return (
     <div className={styles.container}>
       <header>Timer</header>
-      <main>Hello</main>
+      <main>
+        <span />
+        <button onClick={onClickStart}>
+          {state === "stop" ? "Start" : state === "start" ? "Pause" : "Continue"}
+        </button>
+        <button onClick={onClickReset} disabled={state === "stop"}>
+          Reset
+        </button>
+      </main>
     </div>
   );
 }
