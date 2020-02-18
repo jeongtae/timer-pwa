d861103843e689d8946ad7a55d1e9c1ba514f257
jeongtae <rsatang5@naver.com>
2020-02-18 16:40:57 +0900

    Style Timer

--- src/Routes/Timer/Timer.js
+++ src/Routes/Timer/Timer.js
@@ -5,10 +5,41 @@ import { useTimerContext } from "Contexts";
 export default function() {
   const Timer = () => {
     const {
-      states: { left }
+      states: { state, left, total, progress }
     } = useTimerContext();
+    const format = seconds => {
+      const isNegative = seconds < 0;
+      if (isNegative) {
+        seconds = -seconds;
+      }
+      const [h, m, s] = [Math.floor(seconds / 3600), Math.floor(seconds / 60) % 60, seconds % 60];
 
-    return <span>{left}</span>;
+      const ms = `${m < 10 ? "0" : ""}${m}:${s < 10 ? "0" : ""}${s}`;
+      if (h > 0) {
+        return `${isNegative ? "-" : ""}${h < 10 ? "0" : ""}${h}:${ms}`;
+      } else {
+        return `${isNegative ? "-" : ""}${ms}`;
+      }
+    };
+
+    if (state === "stop") {
+      return (
+        <>
+          <span>{format(total)}</span>
+        </>
+      );
+    } else {
+      const spanStyles = [];
+      if (state === "done") spanStyles.push(styles.flash);
+      if (left >= 3600) spanStyles.push(styles.small);
+      return (
+        <>
+          <progress className={state === "done" ? styles.done : null} value={progress} />
+          {/* <span className={state === "done" ? styles.flash : null}>{format(left)}</span> */}
+          <span className={spanStyles.join(" ")}>{format(left)}</span>
+        </>
+      );
+    }
   };
 
   const StartButton = () => {
@@ -18,6 +49,12 @@ export default function() {
     } = useTimerContext();
 
     const texts = { stop: "Start", running: "Pause", pause: "Continue", done: "Restart" };
+    const classes = {
+      stop: styles.green,
+      running: styles.orange,
+      pause: styles.orange,
+      done: styles.green
+    };
     const onClick = () => {
       if (state === "running") {
         pauseTimer();
@@ -26,7 +63,11 @@ export default function() {
       }
     };
 
-    return <button onClick={onClick}>{texts[state]}</button>;
+    return (
+      <button onClick={onClick} className={classes[state]}>
+        {texts[state]}
+      </button>
+    );
   };
 
   const ResetButton = () => {
@@ -52,8 +93,10 @@ export default function() {
       <header>Timer</header>
       <main>
         <Timer />
-        <StartButton />
-        <ResetButton />
+        <div>
+          <ResetButton />
+          <StartButton />
+        </div>
       </main>
     </div>
   );
