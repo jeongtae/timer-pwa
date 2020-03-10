9c3b245cef469d14432edc70742a6ea41d2aaeaf
jeongtae <rsatang5@naver.com>
2020-03-11 00:53:52 +0900

    Update global style fo mobile notch screen

--- src/Components/App/global.style.js
+++ src/Components/App/global.style.js
@@ -1,21 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-
+//
 export default createGlobalStyle`
   ${reset};
-  html,
-  body,
-  #root {
-    position: fixed;
+  html {
+    /* position: relative;
+    top: env(safe-area-inset-top); */
+    height: calc(100% + env(safe-area-inset-top));
+    background: black;
+  }
+  body, #root {
+    height: 100%;
     margin: 0;
     padding: 0;
+  }
+  #root{
     width: 100%;
-    height: 100%;
+    position: fixed;
     background: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.foreground};
     font-family: ${({ theme }) => theme.font};
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    /* touch-action: none; */
     *::selection {
       background-color: transparent;
     }
