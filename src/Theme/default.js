f9d06d29fe0d2a1ce427b3eb4543ba9e72c93e4f
jeongtae <rsatang5@naver.com>
2020-03-25 19:07:14 +0900

    Apply Noto Sans font

--- src/Theme/default.js
+++ src/Theme/default.js
@@ -1,6 +1,5 @@
 export default {
   background: "#222",
   foreground: "#fff",
-  font: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-  "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`
+  font: `"Noto Sans KR", sans-serif`
 };
