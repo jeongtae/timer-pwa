6d7cff0e94a2e32363a1becb4ef274a5c2885f1c
jeongtae <rsatang5@naver.com>
2020-03-30 19:08:33 +0900

    Restructure Timer.js

--- src/Routes/Timer/Timer.js
+++ src/Routes/Timer/Timer.js
@@ -37,7 +37,59 @@ export default function() {
   const currentProgress = Math.min(1.0, elapsed / (total + addedLeft));
   const nextProgress = Math.min(1.0, (elapsed + 1) / (total + addedLeft));
 
-  return (
+  return state === "stop" ? (
+    <>
+      <S.Container blink={state === "done"}>
+        <S.UpperDivision>
+          <S.Pickers ref={pickersRef}>
+            <MemoPicker selectedValue={Time.getHours(total)} onChangeValue={setTimerHours}>
+              {zeroTo23.map((v, i) => (
+                <PickerItem value={v} key={i}>
+                  {v}
+                </PickerItem>
+              ))}
+            </MemoPicker>
+            <S.PickerColon />
+            <MemoPicker selectedValue={Time.getMinutes(total)} onChangeValue={setTimerMinutes}>
+              {zeroTo59.map((v, i) => (
+                <PickerItem value={v} key={i}>
+                  {v}
+                </PickerItem>
+              ))}
+            </MemoPicker>
+            <S.PickerColon />
+            <MemoPicker selectedValue={Time.getSeconds(total)} onChangeValue={setTimerSeconds}>
+              {zeroTo59.map((v, i) => (
+                <PickerItem value={v} key={i}>
+                  {v}
+                </PickerItem>
+              ))}
+            </MemoPicker>
+          </S.Pickers>
+          <S.RecentsBar>
+            {recents.map((recent, index) => (
+              <S.RecentsBarItem key={index}>
+                <S.RecentsBarItemTimeButton onClick={() => setTimer(recent)}>
+                  {Time.format(recent)}
+                </S.RecentsBarItemTimeButton>
+                <S.RecentsBarItemDeleteButton onClick={() => deleteRecent(recent)} />
+              </S.RecentsBarItem>
+            ))}
+          </S.RecentsBar>
+        </S.UpperDivision>
+        <S.LowerDivision>
+          <div />
+          <div />
+          <S.ControlButton disabled={state === "stop"} styled="reset" onClick={resetTimer} />
+          <S.ControlButton
+            disabled={total === 0}
+            styled={state !== "running" ? "start" : "stop"}
+            onClick={state !== "running" ? startTimer : pauseTimer}
+          />
+        </S.LowerDivision>
+      </S.Container>
+    </>
+  ) : (
     <>
       <S.ProgressBackground
         value={state === "pause" ? currentProgress : nextProgress}
@@ -45,69 +97,29 @@ export default function() {
       />
       <S.Container blink={state === "done"}>
         <S.UpperDivision>
-          {state === "stop" ? (
-            <>
-              <S.Pickers ref={pickersRef}>
-                <MemoPicker selectedValue={Time.getHours(total)} onChangeValue={setTimerHours}>
-                  {zeroTo23.map((v, i) => (
-                    <PickerItem value={v} key={i}>
-                      {v}
-                    </PickerItem>
+          <S.Timer
+            digitsCount={4 + (leftHours > 0 && leftHours.toString().length)}
+            colonsCount={leftHours > 0 ? 2 : 1}
+            hasMinus={leftIsNegative}
+          >
+            {leftIsNegative && <S.TimerMinus />}
+            {leftHours > 0 && (
+              <>
+                {leftHours
+                  .toString()
+                  .split("")
+                  .map((digit, idx) => (
+                    <S.TimerDigit key={idx} digit={digit}></S.TimerDigit>
                   ))}
-                </MemoPicker>
-                <S.PickerColon />
-                <MemoPicker selectedValue={Time.getMinutes(total)} onChangeValue={setTimerMinutes}>
-                  {zeroTo59.map((v, i) => (
-                    <PickerItem value={v} key={i}>
-                      {v}
-                    </PickerItem>
-                  ))}
-                </MemoPicker>
-                <S.PickerColon />
-                <MemoPicker selectedValue={Time.getSeconds(total)} onChangeValue={setTimerSeconds}>
-                  {zeroTo59.map((v, i) => (
-                    <PickerItem value={v} key={i}>
-                      {v}
-                    </PickerItem>
-                  ))}
-                </MemoPicker>
-              </S.Pickers>
-              <S.RecentsBar>
-                {recents.map((recent, index) => (
-                  <S.RecentsBarItem key={index}>
-                    <S.RecentsBarItemTimeButton onClick={() => setTimer(recent)}>
-                      {Time.format(recent)}
-                    </S.RecentsBarItemTimeButton>
-                    <S.RecentsBarItemDeleteButton onClick={() => deleteRecent(recent)} />
-                  </S.RecentsBarItem>
-                ))}
-              </S.RecentsBar>
-            </>
-          ) : (
-            <S.Timer
-              digitsCount={4 + (leftHours > 0 && leftHours.toString().length)}
-              colonsCount={leftHours > 0 ? 2 : 1}
-              hasMinus={leftIsNegative}
-            >
-              {leftIsNegative && <S.TimerMinus />}
-              {leftHours > 0 && (
-                <>
-                  {leftHours
-                    .toString()
-                    .split("")
-                    .map((digit, idx) => (
-                      <S.TimerDigit key={idx} digit={digit}></S.TimerDigit>
-                    ))}
-                  <S.TimerColon />
-                </>
-              )}
-              <S.TimerDigit digit={Math.floor(leftMinutes / 10)} />
-              <S.TimerDigit digit={leftMinutes % 10} />
-              <S.TimerColon />
-              <S.TimerDigit digit={Math.floor(leftSeconds / 10)} />
-              <S.TimerDigit digit={leftSeconds % 10} />
-            </S.Timer>
-          )}
+                <S.TimerColon />
+              </>
+            )}
+            <S.TimerDigit digit={Math.floor(leftMinutes / 10)} />
+            <S.TimerDigit digit={leftMinutes % 10} />
+            <S.TimerColon />
+            <S.TimerDigit digit={Math.floor(leftSeconds / 10)} />
+            <S.TimerDigit digit={leftSeconds % 10} />
+          </S.Timer>
         </S.UpperDivision>
         <S.LowerDivision>
           {state === "running" ? (
