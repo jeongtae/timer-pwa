5b1d0a8e85b5c1f4198633324771374d7bd63ae4
jeongtae <rsatang5@naver.com>
2020-03-31 23:40:15 +0900

    Update ProgressBackground position

--- src/Pages/Timer/Timer.js
+++ src/Pages/Timer/Timer.js
@@ -29,78 +29,80 @@ export default function() {
   const nextProgress = Math.min(1.0, (elapsed + 1) / (total + addedLeft));
 
   return (
-    <S.Container>
+    <>
       <S.ProgressBackground
         value={state === "pause" ? currentProgress : nextProgress}
         disabled={state === "stop"}
       />
-      <S.UpperDivision>
-        {state === "stop" ? (
-          <>
-            <TimePicker
-              selectedHours={Time.getHours(total)}
-              selectedMinutes={Time.getMinutes(total)}
-              selectedSeconds={Time.getSeconds(total)}
-              onChangeHours={setTimerHours}
-              onChangeMinutes={setTimerMinutes}
-              onChangeSeconds={setTimerSeconds}
-            />
-            <S.RecentsBar>
-              {recents.map((recent, index) => (
-                <S.RecentsBarItem key={index}>
-                  <S.RecentsBarItemTimeButton onClick={() => setTimer(recent)}>
-                    {Time.format(recent)}
-                  </S.RecentsBarItemTimeButton>
-                  <S.RecentsBarItemDeleteButton onClick={() => deleteRecent(recent)} />
-                </S.RecentsBarItem>
-              ))}
-            </S.RecentsBar>
-          </>
-        ) : (
-          <S.Timer
-            digitsCount={4 + (leftHours > 0 && leftHours.toString().length)}
-            colonsCount={leftHours > 0 ? 2 : 1}
-            hasMinus={leftIsNegative}
-          >
-            {leftIsNegative && <S.TimerMinus />}
-            {leftHours > 0 && (
-              <>
-                {leftHours
-                  .toString()
-                  .split("")
-                  .map((digit, idx) => (
-                    <S.TimerDigit key={idx} digit={digit}></S.TimerDigit>
-                  ))}
-                <S.TimerColon />
-              </>
-            )}
-            <S.TimerDigit digit={Math.floor(leftMinutes / 10)} />
-            <S.TimerDigit digit={leftMinutes % 10} />
-            <S.TimerColon />
-            <S.TimerDigit digit={Math.floor(leftSeconds / 10)} />
-            <S.TimerDigit digit={leftSeconds % 10} />
-          </S.Timer>
-        )}
-      </S.UpperDivision>
-      <S.LowerDivision>
-        {state === "running" ? (
-          <>
-            <S.ControlButton styled="minus" onClick={() => addLeft(-10)} />
-            <S.ControlButton styled="plus" onClick={() => addLeft(+10)} />
-          </>
-        ) : (
-          <>
-            <div />
-            <div />
-          </>
-        )}
-        <S.ControlButton disabled={state === "stop"} styled="reset" onClick={resetTimer} />
-        <S.ControlButton
-          disabled={total === 0}
-          styled={state !== "running" ? "start" : "stop"}
-          onClick={state !== "running" ? startTimer : pauseTimer}
-        />
-      </S.LowerDivision>
-    </S.Container>
+      <S.Container>
+        <S.UpperDivision>
+          {state === "stop" ? (
+            <>
+              <TimePicker
+                selectedHours={Time.getHours(total)}
+                selectedMinutes={Time.getMinutes(total)}
+                selectedSeconds={Time.getSeconds(total)}
+                onChangeHours={setTimerHours}
+                onChangeMinutes={setTimerMinutes}
+                onChangeSeconds={setTimerSeconds}
+              />
+              <S.RecentsBar>
+                {recents.map((recent, index) => (
+                  <S.RecentsBarItem key={index}>
+                    <S.RecentsBarItemTimeButton onClick={() => setTimer(recent)}>
+                      {Time.format(recent)}
+                    </S.RecentsBarItemTimeButton>
+                    <S.RecentsBarItemDeleteButton onClick={() => deleteRecent(recent)} />
+                  </S.RecentsBarItem>
+                ))}
+              </S.RecentsBar>
+            </>
+          ) : (
+            <S.Timer
+              digitsCount={4 + (leftHours > 0 && leftHours.toString().length)}
+              colonsCount={leftHours > 0 ? 2 : 1}
+              hasMinus={leftIsNegative}
+            >
+              {leftIsNegative && <S.TimerMinus />}
+              {leftHours > 0 && (
+                <>
+                  {leftHours
+                    .toString()
+                    .split("")
+                    .map((digit, idx) => (
+                      <S.TimerDigit key={idx} digit={digit}></S.TimerDigit>
+                    ))}
+                  <S.TimerColon />
+                </>
+              )}
+              <S.TimerDigit digit={Math.floor(leftMinutes / 10)} />
+              <S.TimerDigit digit={leftMinutes % 10} />
+              <S.TimerColon />
+              <S.TimerDigit digit={Math.floor(leftSeconds / 10)} />
+              <S.TimerDigit digit={leftSeconds % 10} />
+            </S.Timer>
+          )}
+        </S.UpperDivision>
+        <S.LowerDivision>
+          {state === "running" ? (
+            <>
+              <S.ControlButton styled="minus" onClick={() => addLeft(-10)} />
+              <S.ControlButton styled="plus" onClick={() => addLeft(+10)} />
+            </>
+          ) : (
+            <>
+              <div />
+              <div />
+            </>
+          )}
+          <S.ControlButton disabled={state === "stop"} styled="reset" onClick={resetTimer} />
+          <S.ControlButton
+            disabled={total === 0}
+            styled={state !== "running" ? "start" : "stop"}
+            onClick={state !== "running" ? startTimer : pauseTimer}
+          />
+        </S.LowerDivision>
+      </S.Container>
+    </>
   );
 }
